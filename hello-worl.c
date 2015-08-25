c2124017885f60ca91c6b4da3c19ac1c56c9170d
Patrick Percot <patrick.percot@morbihan.fr>
2015-08-25 15:27:38 +0200

    Add destruction of the world

--- hello-worl.c
+++ hello-worl.c
@@ -3,5 +3,7 @@
 int main(int argc, void * argv) {
 	printf ("Hello World");
 	// Make some stuff to destroy the world
+	
+	// Here the world is completely destroyed. Program ends, due to the lack of a computer to run on
 	printf ("Good Bye Cruel World");
 }
