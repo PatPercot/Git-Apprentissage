b942aa01205d733abad81af123cdcbaf91bd27b1
Patrick Percot <patrick.percot@morbihan.fr>
2015-08-25 17:51:41 +0200

    Added the creation of the world

--- hello-worl.c
+++ hello-worl.c
@@ -2,6 +2,8 @@
 
 int main(int argc, void * argv) {
 	printf ("Hello World");
+	// First of all : create a new world
+	
 	// Make some stuff to destroy the world
 	
 	// Here the world is completely destroyed. Program ends, due to the lack of a computer to run on
