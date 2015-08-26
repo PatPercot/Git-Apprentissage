8dafc1461fc3671e20e8eadd9831c69dd1345380
Patrick Percot <patrick.percot@morbihan.fr>
2015-08-26 15:00:30 +0200

    Correction issue01 : the world was not destroyed at the end of the story

--- hello-worl.c
+++ hello-worl.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+void destroyWorld() {
+	printf("BOOOOM");
+}
+
 int main(int argc, void * argv) {
 	printf ("Hello World");
 	// First of all : create a new world
@@ -13,7 +17,7 @@ int main(int argc, void * argv) {
 	// Some tribes begin to disappear
 	
 	// Make some stuff to destroy the world
-	
+	destroyWorld();
 	// Here the world is completely destroyed. Program ends, due to the lack of a computer to run on
 	printf ("Good Bye Cruel World");
 }
