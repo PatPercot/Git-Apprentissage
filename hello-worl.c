20243de48708489d71fa381ba792b65269795b76
Patrick Percot <patrick.percot@morbihan.fr>
2015-08-26 14:42:14 +0200

    Creation of humans and war

--- hello-worl.c
+++ hello-worl.c
@@ -4,6 +4,14 @@ int main(int argc, void * argv) {
 	printf ("Hello World");
 	// First of all : create a new world
 	
+	// Populate the world with humans
+	
+	// Humans begin to build some things
+	
+	// Humans of different tribes begin to fight 
+	
+	// Some tribes begin to disappear
+	
 	// Make some stuff to destroy the world
 	
 	// Here the world is completely destroyed. Program ends, due to the lack of a computer to run on
