39811a984b8d1e3934dcf26d6fc38718a353737b
Patrick Percot <patrick.percot@morbihan.fr>
2015-08-26 14:33:55 +0200

    Create animals in the very new world

--- hello-worl.c
+++ hello-worl.c
@@ -4,6 +4,8 @@ int main(int argc, void * argv) {
 	printf ("Hello World");
 	// First of all : create a new world
 	
+	// Populate the world with animals : everything is quiet
+	
 	// Make some stuff to destroy the world
 	
 	// Here the world is completely destroyed. Program ends, due to the lack of a computer to run on
